e91ba46037ac8152435e58d5109fff49211b2d67
Jay Versluis <jay@versluis.com>
2015-09-30 23:10:58 -0400

    added Live Photo View and Image Picker

--- Live Photos/ViewController.m
+++ Live Photos/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+@import Photos;
+@import PhotosUI;
+@import MobileCoreServices;
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @end
 
@@ -17,11 +20,72 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)grabLivePhoto:(id)sender {
+    
+    // create an image picker
+    UIImagePickerController *picker = [[UIImagePickerController alloc]init];
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.allowsEditing = NO;
+    picker.delegate = self;
+    
+    // make sure we include Live Photos (otherwise we'll only get UIImages)
+    NSArray *mediaTypes = @[(NSString *)kUTTypeImage, (NSString *)kUTTypeLivePhoto];
+    picker.mediaTypes = mediaTypes;
+    
+    // bring up the picker
+    [self presentViewController:picker animated:YES completion:nil];
+    
+}
+
+# pragma mark - Image Picker Delegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // dismiss the picker
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    // check if this is a Live Image, otherwise present a warning
+    PHLivePhoto *photo = [info objectForKey:UIImagePickerControllerLivePhoto];
+    if (!photo) {
+        [self notLivePhotoWarning];
+        return;
+    }
+    
+    // create a Live Photo View
+    PHLivePhotoView *photoView = [[PHLivePhotoView alloc]initWithFrame:self.view.bounds];
+    photoView.livePhoto = [info objectForKey:UIImagePickerControllerLivePhoto];
+    photoView.contentMode = UIViewContentModeScaleAspectFit;
+    
+    // bring up the Live Photo View
+    [self.view addSubview:photoView];
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)notLivePhotoWarning {
+    
+    // create an alert view
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Not a Live Photo" message:@"Sadly this is a standard image so we can't show it in our Live Photo VIew. \n\nSorry :-(" preferredStyle:UIAlertControllerStyleAlert];
+    
+    // add a single action
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:action];
+    
+    // and display it
+    [self presentViewController:alert animated:YES completion:nil];
+    
 }
 
 @end
